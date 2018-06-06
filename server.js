d08e72414bf6ade9db96c2ce1064d7dafa7fee1f
“juan <juancamilo971218@gmail.com>
2018-06-05 22:52:57 -0500

    Commit third 17

--- server.js
+++ server.js
@@ -143,7 +143,7 @@ app.get('/', (req, res) => {
 
 // URL para eliminar un usuario
 // http://127.0.0.1:5000/users
-app.post('/users', (req, res) => {
+app.post('/login', (req, res) => {
      let data = req.body;
     let login = [{searchUser: false,id: 0,username: '',pass: '',name: '',email: ''}];
 
